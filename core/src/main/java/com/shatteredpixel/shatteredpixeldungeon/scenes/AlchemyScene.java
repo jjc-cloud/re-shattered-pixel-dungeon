2e654531b3a45ee370f92f705cc17f6d0d67839f
MyProject <myproject@163.com>
2026-09-07 17:21:33 +0800

    收紧实验炼金能量控件间距

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/AlchemyScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/AlchemyScene.java
@@ -420,7 +420,7 @@ public class AlchemyScene extends PixelScene {
 			}
 		};
 		float experimentalEnergyCenter = combines[0].left() + combines[0].width()/2f;
-		experimentalEnergyMinus.setRect(experimentalEnergyCenter - 34, inputs[0].top() + 8, 14, 12);
+		experimentalEnergyMinus.setRect(experimentalEnergyCenter - 23, inputs[0].top() + 8, 14, 12);
 		experimentalEnergyMinus.textColor(0xFFFF00);
 		add(experimentalEnergyMinus);
 
@@ -435,7 +435,7 @@ public class AlchemyScene extends PixelScene {
 				updateState();
 			}
 		};
-		experimentalEnergyPlus.setRect(experimentalEnergyCenter + 20, inputs[0].top() + 8, 14, 12);
+		experimentalEnergyPlus.setRect(experimentalEnergyCenter + 9, inputs[0].top() + 8, 14, 12);
 		experimentalEnergyPlus.textColor(0xFFFF00);
 		add(experimentalEnergyPlus);
 
@@ -785,7 +785,7 @@ public class AlchemyScene extends PixelScene {
 		experimentalEnergyPlus.enable(experimentalEnergy < availableEnergy());
 		experimentalEnergyText.text(Integer.toString(experimentalEnergy));
 		experimentalEnergyText.setPos(
-				experimentalEnergyMinus.left() + 34 - experimentalEnergyText.width()/2f,
+				experimentalEnergyMinus.left() + 25 - experimentalEnergyText.width()/2f,
 				experimentalEnergyMinus.top() + (experimentalEnergyMinus.height() - experimentalEnergyText.height())/2f);
 	}
 	
